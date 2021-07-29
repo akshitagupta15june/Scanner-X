68f53f0ee5769bc2882ea3147ac84bfbb996f4d4
AKSHITA GUPTA <57909583+akshitagupta15june@users.noreply.github.com>
2021-07-29 10:39:11 +0530

    Update scannerx.py

--- scannerx.py
+++ scannerx.py
@@ -2,3 +2,10 @@ import cv2
 import numpy as np
 import glob
 from pathlib import Path
+
+path=Path(“file complete path”)
+image=[]
+count=0
+for imagepath in path.glob(“*.png”):
+count=count+1
+image=cv2.imread(str(imagepath))
