19d69cfd0675bc52675cfc58bad231ab0b56961a
AKSHITA GUPTA <57909583+akshitagupta15june@users.noreply.github.com>
2021-08-03 10:42:34 +0530

    Update scannerx.py

--- scannerx.py
+++ scannerx.py
@@ -1,140 +1,40 @@
-import numpy as np
 import cv2
-import re
-from matplotlib import pyplot as plt
-import warnings
-warnings.filterwarnings("ignore", category=np.VisibleDeprecationWarning)
+import numpy as np
+import mapper
+image=cv2.imread("inp.jpg")   #read in the image
+image=cv2.resize(image,(1300,800)) #resizing because opencv does not work well with bigger images
+orig=image.copy()
 
-path = "C:/Users/AKSHITA GUPTA/Desktop/Face-Morphing-master/code/"
-image = cv2.imread("C:/Users/AKSHITA GUPTA/Desktop/Face-Morphing-master/code/dejavu.jpg")
+gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)  #RGB To Gray Scale
+cv2.imshow("Title",gray)
 
+blurred=cv2.GaussianBlur(gray,(5,5),0)  #(5,5) is the kernel size and 0 is sigma that determines the amount of blur
+cv2.imshow("Blur",blurred)
 
-# ## **Use Gaussian Blurring combined with Adaptive Threshold**
-
-def blur_and_threshold(gray):
-    gray = cv2.GaussianBlur(gray, (3, 3), 2)
-    threshold = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
-    threshold = cv2.fastNlMeansDenoising(threshold, 11, 31, 9)
-    return threshold
+edged=cv2.Canny(blurred,30,50)  #30 MinThreshold and 50 is the MaxThreshold
+cv2.imshow("Canny",edged)
 
 
+contours,hierarchy=cv2.findContours(edged,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)  #retrieve the contours as a list, with simple apprximation model
+contours=sorted(contours,key=cv2.contourArea,reverse=True)
+
+#the loop extracts the boundary contours of the page
+for c in contours:
+    p=cv2.arcLength(c,True)
+    approx=cv2.approxPolyDP(c,0.02*p,True)
+
+    if len(approx)==4:
+        target=approx
+        break
+approx=mapper.mapp(target) #find endpoints of the sheet
+
+pts=np.float32([[0,0],[800,0],[800,800],[0,800]])  #map to 800*800 target window
+
+op=cv2.getPerspectiveTransform(approx,pts)  #get the top or bird eye view effect
+dst=cv2.warpPerspective(orig,op,(800,800))
 
 
-def biggest_contour(contours, min_area):
-    biggest = None
-    max_area = 0
-    biggest_n = 0
-    approx_contour = None
-    for n, i in enumerate(contours):
-        area = cv2.contourArea(i)
-
-        if area > min_area / 10:
-            peri = cv2.arcLength(i, True)
-            approx = cv2.approxPolyDP(i, 0.02 * peri, True)
-            if area > max_area and len(approx) == 4:
-                biggest = approx
-                max_area = area
-                biggest_n = n
-                approx_contour = approx
-
-    return biggest_n, approx_contour
-
-
-def order_points(pts):
-
-    pts = pts.reshape(4, 2)
-    rect = np.zeros((4, 2), dtype="float32")
-    s = pts.sum(axis=1)
-    rect[0] = pts[np.argmin(s)]
-    rect[2] = pts[np.argmax(s)]
-    diff = np.diff(pts, axis=1)
-    rect[1] = pts[np.argmin(diff)]
-    rect[3] = pts[np.argmax(diff)]
-    return rect
-
-
-
-
-
-def four_point_transform(image, pts):
-
-    rect = order_points(pts)
-    (tl, tr, br, bl) = rect
-
-
-    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
-    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
-    maxWidth = max(int(widthA), int(widthB))
-    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
-    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
-    maxHeight = max(int(heightA), int(heightB))
-    dst = np.array([
-        [0, 0],
-        [maxWidth - 1, 0],
-        [maxWidth - 1, maxHeight - 1],
-        [0, maxHeight - 1]], dtype="float32")
-    M = cv2.getPerspectiveTransform(rect, dst)
-    warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
-
-    # return the warped image
-    return warped
-
-
-def transformation(image):
-    image = image.copy()
-    height, width, channels = image.shape
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    image_size = gray.size
-
-    threshold = blur_and_threshold(gray)
-    edges = cv2.Canny(threshold, 50, 150, apertureSize=7)
-    contours, hierarchy = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    simplified_contours = []
-
-    for cnt in contours:
-        hull = cv2.convexHull(cnt)
-        simplified_contours.append(cv2.approxPolyDP(hull,
-                                                    0.001 * cv2.arcLength(hull, True), True))
-    simplified_contours = np.array(simplified_contours)
-    biggest_n, approx_contour = biggest_contour(simplified_contours, image_size)
-
-    threshold = cv2.drawContours(image, simplified_contours, biggest_n, (0, 255, 0), 1)
-
-    dst = 0
-    if approx_contour is not None and len(approx_contour) == 4:
-        approx_contour = np.float32(approx_contour)
-        dst = four_point_transform(threshold, approx_contour)
-    croppedImage = dst
-    return croppedImage
-
-
-# **Increase the brightness of the image by playing with the "V" value (from HSV)**
-
-def increase_brightness(img, value=30):
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    h, s, v = cv2.split(hsv)
-    lim = 255 - value
-    v[v > lim] = 255
-    v[v <= lim] += value
-    final_hsv = cv2.merge((h, s, v))
-    img = cv2.cvtColor(final_hsv, cv2.COLOR_HSV2BGR)
-    return img
-
-
-# **Sharpen the image using Kernel Sharpening Technique**
-
-
-def final_image(rotated):
-    # Create our shapening kernel, it must equal to one eventually
-    kernel_sharpening = np.array([[0, -1, 0],
-                                  [-1, 5, -1],
-                                  [0, -1, 0]])
-    # applying the sharpening kernel to the input image & displaying it.
-    sharpened = cv2.filter2D(rotated, -1, kernel_sharpening)
-    sharpened = increase_brightness(sharpened, 30)
-    return sharpened
-
-
-blurred_threshold = transformation(image)
-cleaned_image = final_image(blurred_threshold)
-cv2.imwrite(path + "Final.jpg", cleaned_image)
+cv2.imshow("Scanned",dst)
+# press q or Esc to close
+cv2.waitKey(0)
+cv2.destroyAllWindows()
